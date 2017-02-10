6bbae4b2181c2cb7f165f38ae0148e5bd2541618
danielrth <danielrth9@hotmail.com>
2017-02-10 09:42:55 +0800

    add timer

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ React.render(
   <App />,
   document.getElementById('main')
 );
+
+var TimerExample = require('./components/Timer');
+
+React.render(
+    <TimerExample start={Date.now()} />,
+    document.getElementById('timer')
+);
